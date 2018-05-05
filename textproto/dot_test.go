e900d3ea911900ad01009b664ce7b13369cbbcff
Lukas Dietrich <lukas@lukasdietrich.com>
2018-05-05 13:48:42 +0200

    Add test for dot reader

---
+++ textproto/dot_test.go
@@ -0,0 +1,72 @@
+// Copyright (C) 2018  Lukas Dietrich <lukas@lukasdietrich.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package textproto
+
+import (
+	"bytes"
+	"io/ioutil"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func joinLines(lines []string) string {
+	// ensure final <CR> <LF>
+	lines = append(lines, "")
+	return strings.Join(lines, "\r\n")
+}
+
+func TestDotReader(t *testing.T) {
+	sequence := []string{
+		"first line",
+		"normal line",
+		".with a dot",
+		"",
+		"..",
+		".",
+		"last line",
+	}
+
+	decoded := []string{
+		"normal line",
+		"with a dot",
+		"",
+		".",
+	}
+
+	buffer := bytes.NewBufferString(joinLines(sequence))
+	reader := newReader(buffer)
+
+	{ // test before dot encoded block
+		line, err := reader.ReadLine()
+		assert.Nil(t, err)
+		assert.EqualValues(t, sequence[0], line)
+	}
+
+	{ // test a handful of dot encoded lines
+		text, err := ioutil.ReadAll(reader.DotReader())
+		assert.Nil(t, err)
+		assert.EqualValues(t, joinLines(decoded), text)
+	}
+
+	{ // resume after dot encoded block
+		line, err := reader.ReadLine()
+		assert.Nil(t, err)
+		assert.EqualValues(t, sequence[6], line)
+	}
+
+}
