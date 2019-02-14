03bb247387b8f2a78171e5ea4b1f2568aa6f6cc9
Lukas Dietrich <lukas@lukasdietrich.com>
2019-02-14 04:28:13 +0100

    Refactor certificate sources to an interface

--- config/tls.go
+++ config/tls.go
@@ -24,9 +24,14 @@ import (
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
-type certFunc func(*tls.ClientHelloInfo) (*tls.Certificate, error)
+type certSource interface {
+	time() (time.Time, error)
+	load() (*tls.Certificate, error)
+}
 
 type TLS struct {
 	Source string
@@ -41,14 +46,27 @@ type TLS struct {
 }
 
 func (t *TLS) MakeTLSConfig() (*tls.Config, error) {
-	var f certFunc
+	var (
+		log = logrus.WithField("prefix", "config-tls")
+
+		source   certSource
+		lastCert *tls.Certificate
+		lastTime time.Time
+		lock     sync.Mutex
+	)
 
 	switch strings.ToLower(t.Source) {
 	case "files":
-		f = certByFile(t.Crt, t.Key)
+		source = &filesCertSource{
+			crtFile: t.Crt,
+			keyFile: t.Key,
+		}
 
 	case "traefik":
-		f = certByTraefikJson(t.Acme, t.Domain)
+		source = &traefikCertSource{
+			acmeFile: t.Acme,
+			domain:   t.Domain,
+		}
 
 	case "":
 		return nil, nil
@@ -57,121 +75,121 @@ func (t *TLS) MakeTLSConfig() (*tls.Config, error) {
 		return nil, fmt.Errorf("unknown certificate source: %s", t.Source)
 	}
 
-	return &tls.Config{GetCertificate: f}, nil
-}
+	return &tls.Config{
+		GetCertificate: func(*tls.ClientHelloInfo) (*tls.Certificate, error) {
+			lock.Lock()
+			defer lock.Unlock()
 
-func certByFile(crtFile, keyFile string) certFunc {
-	var (
-		files    = []string{crtFile, keyFile}
-		mutex    sync.Mutex
-		lastCert *tls.Certificate
-		lastTime time.Time
-	)
-
-	return func(*tls.ClientHelloInfo) (*tls.Certificate, error) {
-		mutex.Lock()
-		defer mutex.Unlock()
-
-		if lastCert != nil {
-			for _, file := range files {
-				info, err := os.Stat(file)
-				if err != nil {
-					return nil, err
-				}
-
-				if info.ModTime().After(lastTime) {
-					goto load
-				}
-			}
-
-			return lastCert, nil
-		}
-
-	load:
-		cert, err := tls.LoadX509KeyPair(crtFile, keyFile)
-		if err != nil {
-			return nil, err
-		}
-
-		lastCert = &cert
-		lastTime = time.Now()
-
-		return lastCert, nil
-	}
-}
-
-func certByTraefikJson(acmeFile string, domain string) certFunc {
-	var (
-		base64   = base64.StdEncoding
-		mutex    sync.Mutex
-		lastCert *tls.Certificate
-		lastTime time.Time
-	)
-
-	return func(*tls.ClientHelloInfo) (*tls.Certificate, error) {
-		mutex.Lock()
-		defer mutex.Unlock()
-
-		if lastCert != nil {
-			info, err := os.Stat(acmeFile)
+			newTime, err := source.time()
 			if err != nil {
 				return nil, err
 			}
 
-			if info.ModTime().After(lastTime) {
-				goto load
+			if newTime.After(lastTime) {
+				newCert, err := source.load()
+				if err != nil {
+					return nil, err
+				}
+
+				lastTime = newTime
+				lastCert = newCert
+
+				log.Debug("reloaded certificate")
 			}
 
 			return lastCert, nil
-		}
+		},
+	}, nil
+}
 
-	load:
-		var data struct {
-			Entries []struct {
-				Domain struct {
-					Main string `json:"main"`
-				} `json:"domain"`
+type filesCertSource struct {
+	crtFile string
+	keyFile string
+}
 
-				Pem string `json:"certificate"`
-				Key string `json:"key"`
-			} `json:"certificates"`
-		}
+func (s *filesCertSource) time() (time.Time, error) {
+	var t time.Time
 
-		f, err := os.Open(acmeFile)
+	for _, file := range [...]string{s.crtFile, s.keyFile} {
+		info, err := os.Stat(file)
 		if err != nil {
-			return nil, err
+			return t, err
 		}
 
-		defer f.Close()
-
-		if err := json.NewDecoder(f).Decode(&data); err != nil {
-			return nil, err
+		if info.ModTime().After(t) {
+			t = info.ModTime()
 		}
-
-		for _, entry := range data.Entries {
-			if entry.Domain.Main == domain {
-				crtPem, err := base64.DecodeString(entry.Pem)
-				if err != nil {
-					return nil, err
-				}
-
-				keyPem, err := base64.DecodeString(entry.Key)
-				if err != nil {
-					return nil, err
-				}
-
-				cert, err := tls.X509KeyPair(crtPem, keyPem)
-				if err != nil {
-					return nil, err
-				}
-
-				lastCert = &cert
-				lastTime = time.Now()
-
-				return lastCert, nil
-			}
-		}
-
-		return nil, fmt.Errorf("no certificate for domain=%s found", domain)
 	}
+
+	return t, nil
+}
+
+func (s *filesCertSource) load() (*tls.Certificate, error) {
+	cert, err := tls.LoadX509KeyPair(s.crtFile, s.keyFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cert, nil
+}
+
+type traefikCertSource struct {
+	acmeFile string
+	domain   string
+}
+
+func (s *traefikCertSource) time() (time.Time, error) {
+	info, err := os.Stat(s.acmeFile)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	return info.ModTime(), nil
+}
+
+func (s *traefikCertSource) load() (*tls.Certificate, error) {
+	var data struct {
+		Entries []struct {
+			Domain struct {
+				Main string `json:"main"`
+			} `json:"domain"`
+
+			Crt string `json:"certificate"`
+			Key string `json:"key"`
+		} `json:"certificates"`
+	}
+
+	f, err := os.Open(s.acmeFile)
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
+	if err := json.NewDecoder(f).Decode(&data); err != nil {
+		return nil, err
+	}
+
+	for _, entry := range data.Entries {
+		if entry.Domain.Main == s.domain {
+			crtPem, err := base64.StdEncoding.DecodeString(entry.Crt)
+			if err != nil {
+				return nil, err
+			}
+
+			keyPem, err := base64.StdEncoding.DecodeString(entry.Key)
+			if err != nil {
+				return nil, err
+			}
+
+			cert, err := tls.X509KeyPair(crtPem, keyPem)
+			if err != nil {
+				return nil, err
+			}
+
+			return &cert, nil
+		}
+	}
+
+	return nil, fmt.Errorf("no certificate for domain=%s found", s.domain)
 }
